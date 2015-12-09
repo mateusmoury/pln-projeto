af49de18a00b3f5930322ecf87dfcc44d24d9b77
Mateus <mmfrb@cin.ufpe.br>
2015-12-09 14:32:20 -0300

    adicionando pre processamento (falta correcao ortografica)

---
+++ preprocessor.py
@@ -0,0 +1,18 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import string
+
+def preprocess(text):
+  stemmer = PorterStemmer()
+  stop = stopwords.words("english")
+  result = word_tokenize(text)
+  result = [stemmer.stem_word(word) for word in result if \
+            word not in stop and \
+            word not in string.punctuation and \
+            word not in string.digits]
+  return result
+
+if __name__ == '__main__':
+  print(preprocess("Hi, my name is Mateus and I hope that you delete some of the text I just wrote"))
+
