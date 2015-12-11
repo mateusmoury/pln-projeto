39866a539084776fa32117d398a9145a03b841aa
Mateus <mmfrb@cin.ufpe.br>
2015-12-11 11:16:20 -0300

    adding spell checker

--- preprocessor.py
+++ preprocessor.py
@@ -1,18 +1,19 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from spellcheck import correct
 import string
 
 def preprocess(text):
   stemmer = PorterStemmer()
   stop = stopwords.words("english")
   result = word_tokenize(text)
-  result = [stemmer.stem_word(word) for word in result if \
+  result = [stemmer.stem_word(correct(word.lower())) for word in result if \
             word not in stop and \
             word not in string.punctuation and \
             word not in string.digits]
   return result
 
 if __name__ == '__main__':
-  print(preprocess("Hi, my name is Mateus and I hope that you delete some of the text I just wrote"))
+  print(preprocess("Hi, my name is Mateus and I hope that you delete some of the text I just wrote and here is a wrng wrd"))
 
